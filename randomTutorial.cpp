881029659363278a265b138de68a2e1256484fd3
Joel Luth <joel.luth@dell.comm>
2021-01-20 15:51:35 -0600

    Use new random sample/hold LFO class

--- randomTutorial.cpp
+++ randomTutorial.cpp
@@ -13,34 +13,32 @@
 ////////////////////////////////////////////////////////////////////////
 //Includes
 #include "usermodfx.h"
-#include "randomtable.h"
 
 #include "chamberlin_filter.hpp"
+#include "random_sample_hold.hpp"
 
 // Defines
 #define FIXED_Q 0.17                   // Do not reach zero nor do not exceed 2. Lower values = more resonance.
 #define TWOPI 6.283185307              // 2*pi constant used by the filter   
 
-#define RANDOM_TABLE_MASK 0x3FF        // The table has 1024 (0x400) values, we simply mask the increment register with this value vs "checking if value >= max then set to 0". 
 #define SAMPLE_RATE 48000
 #define CENTRE_FREQUENCY 1200          // This is the centre frequency of the filter. When the depth is set to 0, the filter will be at this frequency.
                                        // * When version 2.00 of the minilogue xd is released, you could use shift+depth to set this...
 #define INITIAL_FREQUENCY 800          // On effect load this is the frequency to assign to the filter initially
 #define INITIAL_FREQUENCY_DEVIATION 800 // On effect load this is the 'depth' amout to assign initially
-#define INITIAL_LFO_FREQUENCY 6        // On effect load this is the starting LFO rate
                               
 #define MAX_FREQUENCY_DEVIATION 1100   // This should not exceed the centre frequency! This sets how much the frequency will deviate when the depth knob is at 'full'.
+#define INITIAL_LFO_FREQUENCY 6        // On effect load this is the starting LFO rate
 #define MAX_LFO_RATE 20                // This is the maximum rate the LFO for the "sample and hold/randomizer" will run
 
-float phaseAccumLFO1 = 0;              // LFO phase accumulator - this will count from 0 to (just under)1 , cycling at the LFO rate.
 float LFORate1 = INITIAL_LFO_FREQUENCY;// LFO frequency 
 float valTime = 0;                     // Float value of the "Time" knob from 0 to 1
 float valDepth = 0;                    // Float value of the "Depth" knob from 0 to 1
 float effectDepth = 0;			// Pre-calculated effect deviation (calculated at the knob turn event)
-uint16_t randomIndex = 0;              // Current index into the random table. 
 static float Q = FIXED_Q;               // Filter resonance. This is defined as 1/Q, so a value of 2 = no resonance, and lower values = more resonance. A value of 0 may silence the filter however.
 static float F = 0;                     // Filter cutoff frequency, although not quite expressed as 1:1 for frequency
 static ChamberlinFilter filter;			// Filter object
+static RandomSampleHold lfo;			// Random sample & hold LFO object
 
 
 ////////////////////////////////////////////////////////////////////////
@@ -50,15 +48,9 @@ void MODFX_INIT(uint32_t platform, uint32_t api)
 {
    // This occurs once when the mod fx is selected. This is not executed when the effect is enabled / disabled
    
-   // Reset the phase accumulator
-   phaseAccumLFO1 = 0;      
-   
-   // Set a default filter LFO rate
-   LFORate1 = INITIAL_LFO_FREQUENCY;
-   
-   // Initialize the random table index
-   randomIndex = 0; //index in the random table
-   
+   // Initialize the S&H LFO
+   lfo = RandomSampleHold{LFORate1, SAMPLE_RATE};
+
    // Initialize the filter
    filter = ChamberlinFilter{INITIAL_FREQUENCY, FIXED_Q, SAMPLE_RATE};
    effectDepth = INITIAL_FREQUENCY_DEVIATION;   //Initialize the initial effect depth
@@ -115,34 +107,21 @@ void MODFX_PROCESS(const float *main_xn, float *main_yn,
       //  in 48000/16 = 3000hz update rate. The effects however currently use 64 samples, which equals a 750hz update rate. Especially as this effect
       //  doesn't do a whole lot, we will keep the LFO in the DSP loop
       
-      // *As always, the LFO itself could even be in a separate class etc however for this demo it is of courser kept simple. It is also entirely possible 
-      // that doing so also may result in using less CPU time, which is imperative as this is realtime DSP code!
-      
-      phaseAccumLFO1 += (LFORate1 / 48000); //48000 is the sample rate      
+      lfo.TickLfo();    
       
       // Since we want a new random sample each time it loops, we'll check to see if it's overflowed here:
-      if (phaseAccumLFO1 >= 1)
-      {
-         // Restrict the lfo phase accumulator to 0-1
-         phaseAccumLFO1 -= (uint32_t) phaseAccumLFO1;
-         
+      if (lfo.GetPhaseAccum() >= 1)
+      {  
          // Calculate the new filter frequency:
          
          // Get the value from the table. *Note, the random table provided by random.org is a value from 0-1. Again, to save time, I've converted this table
          // to -1 to +1 ((val*2)-1) to save potentially wasted CPU time.
          
-         randomValueF = randTable[randomIndex]; // Get a 'random' value from -1 to 1
+         randomValueF = lfo.GetRandomValue(); // Get a 'random' value from -1 to 1
          
          // Set the filter frequency here
          F = CENTRE_FREQUENCY + (randomValueF*effectDepth); 
 		 filter.SetFc(F);
-         
-         // Increment and roll over the random table index
-         randomIndex++;
-         
-         randomIndex &= RANDOM_TABLE_MASK;   // This is the same as "if value >= # of entries then value = 0" but by using a table size that can be bitmasked
-                                             // (e.g.1,2,4,8,16,32,64,128 etc, ours is 1024), we merely mask off the exceeding bits to again save time. We don't
-                                             // *need* a table with say 1000 or 950 entries, a table of 1024 is perfectly fine.
       }
       
       // Get the input signal from the left channel (note effects are processed chorus->delay->reverb, and the synthesizer is currently mono up until the effects,
@@ -181,7 +160,8 @@ void MODFX_PARAM(uint8_t index, int32_t value)
       valTime = valf; //Store this if we wish to use it later
       
         // Set the LFO rate here, no need to calculate it every time the sample loop runs!
-      LFORate1 = MAX_LFO_RATE * valf; 
+      LFORate1 = MAX_LFO_RATE * valf;
+	  lfo.SetLfoFreq(LFORate1);
       return;
    
    case 1:
